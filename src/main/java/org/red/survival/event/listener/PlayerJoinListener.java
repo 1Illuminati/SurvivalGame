b41827d157d37f20527fca48610be6c8481f71b9
1Illuminati <95470242+1Illuminati@users.noreply.github.com>
2023-09-21 22:17:12 +0900

    sex

--- src/main/java/org/red/survival/event/listener/PlayerJoinListener.java
+++ src/main/java/org/red/survival/event/listener/PlayerJoinListener.java
@@ -1,6 +1,5 @@
 package org.red.survival.event.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,19 +15,16 @@ public class PlayerJoinListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void event(PlayerJoinEvent event) {
         SurvivalGame.sendDebugLog("PlayerJoinEvent: " + event.getPlayer().getName() + " joined the server.");
+        A_Player player = A_Player.getAPlayer(event.getPlayer());
+        ScoreBoardHelper scoreBoardHelper = new ScoreBoardHelper(player.getPlayer());
+        scoreBoardHelper.setTitle("§f§l[V]arallel World");
 
-        Bukkit.getScheduler().runTaskLater(SurvivalGame.getPlugin(), () -> {
-            A_Player player = A_Player.getAPlayer(event.getPlayer());
-            ScoreBoardHelper scoreBoardHelper = new ScoreBoardHelper(player.getPlayer());
-            scoreBoardHelper.setTitle("§f§l[V]arallel World");
-
-            player.addPlayerRunnable(new NamespacedKey(SurvivalGame.getPlugin(), "scoreboard"), aPlayer -> {
-                scoreBoardHelper.setSlot(5, " §f§l플레이어: " + "§7" + player.getName());
-                scoreBoardHelper.setSlot(4, "");
-                scoreBoardHelper.setSlot(3, " §f§l소지금: " + "§7" + player.getEconomyAccount().getBalance());
-                scoreBoardHelper.setSlot(2, "");
-                scoreBoardHelper.setSlot(1, " §f§l직업: " + "§7" + Job.getJobByJobType(JobType.getPlayerJobType(player)).displayName());
-            }, 5);
-        }, 20);
+        player.addPlayerRunnable(new NamespacedKey(SurvivalGame.getPlugin(), "scoreboard"), aPlayer -> {
+            scoreBoardHelper.setSlot(5, " §f§l플레이어: " + "§7" + aPlayer.getName());
+            scoreBoardHelper.setSlot(4, "");
+            scoreBoardHelper.setSlot(3, " §f§l소지금: " + "§7" + aPlayer.getEconomyAccount().getBalance());
+            scoreBoardHelper.setSlot(2, "");
+            scoreBoardHelper.setSlot(1, " §f§l직업: " + "§7" + Job.getJobByJobType(JobType.getPlayerJobType(aPlayer)).displayName());
+        }, 5);
     }
 }
